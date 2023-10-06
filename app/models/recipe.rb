9f2e7905bdb6861a2a3d12c3cab94548ab6e777c
Basim Aljazaeri <30289772+ShiroYaksha90@users.noreply.github.com>
2023-10-06 21:29:03 +0300

    Add belongs to association & user id validation

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,6 @@
 class Recipe < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
   validates :description, presence: true, length: { maximum: 500 }
+  belongs_to :user
+  validates :user_id, presence: true
 end
