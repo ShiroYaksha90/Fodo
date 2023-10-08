56127462b92b337da84b625351d8629677445e39
Basim Aljazaeri <30289772+ShiroYaksha90@users.noreply.github.com>
2023-10-08 03:13:08 +0300

    Update recipes spec

--- spec/requests/recipes_spec.rb
+++ spec/requests/recipes_spec.rb
@@ -1,10 +1,27 @@
 require 'rails_helper'
 
-RSpec.describe "Recipes", type: :request do
-  describe "GET /recipes" do
-    it "works! (now write some real specs)" do
+RSpec.describe 'Recipes', type: :request do
+  describe 'GET /recipes' do
+    before(:each) do
+      @user = User.create(name: 'chef', email: 'chef@example.com')
+      @recipe = @user.recipes.build(name: 'Vegetable saute',
+                                    description: 'Greate vegetable sautee, add vegetable nad oil', user: @user)
+      @recipe.save
+      @recipe2 = @user.recipes.build(name: 'Chicken saute', description: 'Greate Chicken dish', user: @user)
+      @recipe2.save
+    end
+    it 'works! (now write some real specs)' do
       get recipes_path
       expect(response).to have_http_status(200)
+    end
+
+    it 'displays recipes' do
+      get recipes_path
+      expect(response).to render_template(:index)
+      expect(response.body).to include(@recipe.name)
+      expect(response.body).to include(@recipe.description)
+      expect(response.body).to include(@recipe2.name)
+      expect(response.body).to include(@recipe2.description)
     end
   end
 end
