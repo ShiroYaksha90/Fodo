0fc188c27fb1ee3e3bf707027c31244b45ea0801
ShiroYaksha90 <aljazaeribasim@gmail.com>
2023-11-20 21:58:28 +0300

    Add specs for validation and associations

--- spec/models/recipe_ingredient_spec.rb
+++ spec/models/recipe_ingredient_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe RecipeIngredient, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @user = User.create(name: 'Test User', email: 'test@example.com', password: 'password')
+    @recipe = @user.recipes.build(name: 'Test Recipe', description: 'Test Recipe Description')
+    @recipe.save
+    @ingredient = @recipe.ingredients.create(name: 'Test Ingredient')
+  end
+  it 'is valid with valid attributes' do
+    @recipe_ingredient = @recipe.recipe_ingredients.build(ingredient_id: @ingredient.id)
+    expect(@recipe_ingredient).to be_valid
+  end
+  it 'is not valid without a recipe_id' do
+    @recipe_ingredient = @recipe.recipe_ingredients.build(ingredient_id: nil)
+    expect(@recipe_ingredient).to_not be_valid
+  end
+  it 'is not valid without an ingredient_id' do
+    @recipe_ingredient = @recipe.recipe_ingredients.build(ingredient_id: nil)
+    expect(@recipe_ingredient).to_not be_valid
+  end
+  it 'belongs to a recipe' do
+    @recipe_ingredient = @recipe.recipe_ingredients.build(ingredient_id: @ingredient.id)
+    expect(@recipe_ingredient.recipe.name).to eq('Test Recipe')
+  end
+  it 'belongs to an ingredient' do
+    @recipe_ingredient = @recipe.recipe_ingredients.build(ingredient_id: @ingredient.id)
+    expect(@recipe_ingredient.ingredient.name).to eq('Test Ingredient')
+  end
 end
