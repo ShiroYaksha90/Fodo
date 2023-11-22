c5b463004828225e0dc099ae24dfeb404da3a425
ShiroYaksha90 <aljazaeribasim@gmail.com>
2023-11-22 21:31:37 +0300

    Add integration spec for invalid signup

--- spec/features/users_signup_spec.rb
+++ spec/features/users_signup_spec.rb
@@ -14,7 +14,17 @@ RSpec.feature "UsersSignups", type: :feature do
     expect(page).to have_selector('label', text:'Confirm password')
     expect(page).to have_selector('input', id:'user_password_confirmation')
     expect(page).to have_button('Create account')
-
-
+  end
+  scenario "renders signup page with errors" do
+    visit signup_path
+    click_button 'Create account'
+    expect(page).to have_selector('h1', text:'Sign up')
+    expect(page).to have_selector('div.alert.alert-danger.alert-dismissible', text:'Prevented this User from being saved')
+    expect(page).to have_selector('h2.card-title.text-danger', text:'5 errors Prevented this User from being saved:')
+    expect(page).to have_selector('li', text:"Name can't be blank")
+    expect(page).to have_selector('li', text:"Email can't be blank")
+    expect(page).to have_selector('li', text:"Email is invalid")
+    expect(page).to have_selector('li', text:"Password can't be blank")
+    expect(page).to have_selector('li', text:"Password is too short (minimum is 6 characters)")
   end
 end
