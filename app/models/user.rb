c68f166d342a38d495cd3e527e511e1ac3a51f5d
ShiroYaksha90 <aljazaeribasim@gmail.com>
2023-11-23 17:04:57 +0300

    Add allow_nil validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   #        :recoverable, :rememberable, :validatable
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
