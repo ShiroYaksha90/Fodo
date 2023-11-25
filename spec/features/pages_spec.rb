2d1805e7c4542f950d61c42409217a37bdd90947
ShiroYaksha90 <aljazaeribasim@gmail.com>
2023-11-25 20:26:53 +0300

    Add specs for navigation links

--- spec/features/pages_spec.rb
+++ spec/features/pages_spec.rb
@@ -6,6 +6,9 @@ RSpec.feature 'Pages', type: :feature do
     expect(page).to have_selector('h1', text: 'Welcome to Foodo App')
     expect(page).to have_selector('h2', text: 'The best recipe app')
     expect(page).to have_selector('a', text: 'Sign up or Log in')
+    expect(page).to have_selector('a', text: 'Foodo')
+    expect(page).to have_selector('a', text: 'Recipes')
+    expect(page).to have_selector('a', text: 'Chefs')
     expect(page).to have_link('Sign up or Log in', href: signup_path)
   end
 end
