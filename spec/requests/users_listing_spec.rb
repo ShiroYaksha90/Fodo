0c2d2082858285b3d80b3a505b53dc1d7da22c9d
ShiroYaksha90 <aljazaeribasim@gmail.com>
2023-11-25 20:25:55 +0300

    Update file

--- spec/requests/users_listing_spec.rb
+++ spec/requests/users_listing_spec.rb
@@ -7,8 +7,8 @@ RSpec.describe "UsersListings", type: :request do
       @user2 = User.create!(name: "Example User2", email: "example2@exampel.com", password: "password", password_confirmation: "password")
       get users_path
       expect(response).to have_http_status(200)
-      expect(response.body).to include(@user.name)
-      expect(response.body).to include(@user2.name)
+      expect(response.body).to include(@user.name.capitalize)
+      expect(response.body).to include(@user2.name.capitalize)
       expect(response.body).to include(@user.recipes.count.to_s)
       expect(response.body).to include(@user2.recipes.count.to_s)
     end
