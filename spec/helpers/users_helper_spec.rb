83ba65a05040b0fee19d70c3b9ca6c6a0dffc22b
ShiroYaksha90 <aljazaeribasim@gmail.com>
2023-11-28 22:27:29 +0300

    Add logged_in? method

--- spec/helpers/users_helper_spec.rb
+++ spec/helpers/users_helper_spec.rb
@@ -3,3 +3,7 @@ require 'rails_helper'
 def sign_in_as(user, password)
     post login_path, params: { session: { email: user.email, password: password } }
 end
+
+def logged_in?
+    !session[:user_id].nil?
+end
