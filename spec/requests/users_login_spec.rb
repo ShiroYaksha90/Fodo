4a78ed44a8d862e292e277268af1f67d21dd1fec
ShiroYaksha90 <aljazaeribasim@gmail.com>
2023-11-28 22:24:10 +0300

    Update specs to logged_in user

--- spec/requests/users_login_spec.rb
+++ spec/requests/users_login_spec.rb
@@ -1,10 +1,13 @@
 require 'rails_helper'
-
+require 'helpers/users_helper_spec'
 RSpec.describe "UsersLogins", type: :request do
   describe "GET /users_logins" do
     it "rejects invalid login" do
       get login_path
+      expect(response).to render_template('new')
       post login_path, params: { session: { email: "", password: "" } }
+      expect(response).to render_template('new')
+      expect(logged_in?).to be_falsey
       expect(flash[:danger]).to be_present
       get root_path
       expect(flash[:danger]).to_not be_present
@@ -12,10 +15,18 @@ RSpec.describe "UsersLogins", type: :request do
     it "accepts valid login" do
       @user = User.create!(name: "Example User", email: "example@example.com", password: "password", password_confirmation: "password")
       get login_path
+      expect(response).to render_template('new')
       post login_path, params: { session: { email: @user.email, password: @user.password } }
-      # expect(flash[:success]).to be_present
-      get user_path(@user)
-      expect(flash[:success]).to_not be_present
+      expect(logged_in?).to be_truthy
+      expect(response).to redirect_to(@user)
+      follow_redirect!
+      expect(response).to render_template('users/show')
+      expect(response.body).to include(@user.name)
+      expect(response.body).to include(@user.email)
+      expect(response.body).to include(@user.recipes.count.to_s)
+      expect(response.body).to include('Logout')
+      expect(response.body).to_not include('Login')
+      expect(response.body).to include("#{@user.name.capitalize}'s Profile")
     end
   end
 end
