989f38064632bf8d7fb0d121316ec7463fc0284d
ShiroYaksha90 <aljazaeribasim@gmail.com>
2023-11-28 22:25:08 +0300

    Add sign_in_as to login user

--- spec/requests/users_listing_spec.rb
+++ spec/requests/users_listing_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
-
+require 'helpers/users_helper_spec'
 RSpec.describe "UsersListings", type: :request do
   describe "GET /users_listings" do
     it "lists all users" do
       @user = User.create!(name: "Example User", email: "example@exampel.com", password: "password", password_confirmation: "password")
       @user2 = User.create!(name: "Example User2", email: "example2@exampel.com", password: "password", password_confirmation: "password")
+      sign_in_as(@user, @user.password)
       get users_path
       expect(response).to have_http_status(200)
       expect(response.body).to include(@user.name.capitalize)
