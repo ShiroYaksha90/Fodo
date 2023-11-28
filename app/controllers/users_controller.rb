2a8a3852520dd2074c6aba9192befaac54761554
ShiroYaksha90 <aljazaeribasim@gmail.com>
2023-11-28 22:27:05 +0300

    Debug & cleanup codebase

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  before_action :require_same_user, only: %i[edit update destroy]
+  before_action :require_user, only: %i[edit update destroy]
   before_action :require_admin, only: [:destroy]
     def index
       @users = User.order(:name).page params[:page]
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
     end
 
     def create
-      @user = User.new(user_params) # Not the final implementation!
+      @user = User.new(user_params)
       if @user.save
         log_in @user
         flash[:success] = "Welcome to the Foodo App!"
@@ -56,7 +56,7 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 
-  def require_same_user
+  def require_user
     return if current_user == @user || current_user.admin?
     flash[:danger] = 'You can only edit or delete your own account'
     redirect_to users_path
