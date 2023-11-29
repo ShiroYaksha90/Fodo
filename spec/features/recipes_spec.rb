64dafb2ec8e8a30af18807fa3a0d3c29c17b9563
ShiroYaksha90 <aljazaeribasim@gmail.com>
2023-11-29 17:30:14 +0300

    Update specs

--- spec/features/recipes_spec.rb
+++ spec/features/recipes_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
-
+require 'helpers/recipes_helper_spec'
 RSpec.feature 'Recipes', type: :feature do
   scenario 'should display recipes' do
     @user = User.create(name: 'chef', email: 'example@example.com', password: 'password')
@@ -14,8 +14,6 @@ RSpec.feature 'Recipes', type: :feature do
     expect(page).to have_content(@recipe.description)
     expect(page).to have_link(@recipe.name, href: recipe_path(@recipe))
     expect(page).to have_selector('img', class: 'gravatar')
-    expect(page).to have_link('Edit', href: edit_recipe_path(@recipe))
-    expect(page).to have_button('Delete', type: 'submit')
     expect(page).to have_content(@recipe2.name)
     expect(page).to have_content(@recipe2.description)
     expect(page).to have_link(@recipe2.name, href: recipe_path(@recipe2))
@@ -24,12 +22,16 @@ RSpec.feature 'Recipes', type: :feature do
 
   scenario 'should display recipe' do
     @user = User.create(name: 'chef', email: 'example@example.com', password: 'password')
+    features_sign_in(@user.email, @user.password)
     @recipe = @user.recipes.build(name: 'Vegetable saute',
                                   description: 'Greate vegetable sautee, add vegetable nad oil')
     @recipe.save
     visit "/recipes/#{@recipe.id}"
     expect(page).to have_selector('h1', text: @recipe.name.to_s)
     expect(page).to have_selector('p', text: @recipe.description.to_s)
+    expect(page).to have_selector('img', class: 'gravatar')
+    expect(page).to have_selector('a', text: 'Edit')
+    expect(page).to have_button('Delete')
     expect(page).to have_selector('a', text: 'Back to recipes list')
   end
 end
