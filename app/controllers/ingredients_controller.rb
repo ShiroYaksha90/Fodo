168d842c375ce2c34ae44462ee331d3ff86854ee
ShiroYaksha90 <aljazaeribasim@gmail.com>
2023-12-01 19:21:30 +0300

    Create ingredients controller

---
+++ app/controllers/ingredients_controller.rb
@@ -0,0 +1,26 @@
+class IngredientsController < ApplicationController
+
+    def new
+
+    end
+
+    def create
+      
+    end
+
+    def edit
+      
+    end
+
+    def update
+      
+    end
+
+    def show
+      
+    end
+
+    def index
+      
+    end
+end
