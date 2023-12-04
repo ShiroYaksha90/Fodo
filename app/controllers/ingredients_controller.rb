8c12d2addf5d2c6dcb786a6e224a9211e191159a
ShiroYaksha90 <aljazaeribasim@gmail.com>
2023-12-04 21:36:37 +0300

    Add require_admin method to restrict edit & create

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,6 +1,6 @@
 class IngredientsController < ApplicationController
   before_action :set_ingredient, only: %i[show edit update]
-
+  before_action :require_admin, except: %i[index show]
   def new
   @ingredient = Ingredient.new
   end
@@ -44,5 +44,12 @@ class IngredientsController < ApplicationController
 
   def ingredient_params
     params.require(:ingredient).permit(:name)
+  end
+
+  def require_admin
+    if !logged_in? || (logged_in? and !current_user.admin)
+      flash[:danger] = 'Only admin users can preform this action'
+      redirect_to ingredients_path
+    end
   end
 end
