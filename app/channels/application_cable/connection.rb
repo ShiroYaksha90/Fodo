3ce1fc649ba63fd04a384a57920a9d239a7330ef
ShiroYaksha90 <aljazaeribasim@gmail.com>
2023-12-09 22:30:57 +0300

    Add connection methods

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,18 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    dentified_by :current_user
+    def connect; end
+
+    def disconnect; end
+
+    protected
+
+    def find_current_user
+      if (current_user = User.find_by(id: cookies.signed[:user_id]))
+        current_user
+      else
+        reject_unautherized_connection
+      end
+    end
   end
 end
