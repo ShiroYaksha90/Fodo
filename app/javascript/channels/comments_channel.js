f142e2a7e5a33cc5878ead3bde01da85f7bfb58d
ShiroYaksha90 <aljazaeribasim@gmail.com>
2023-12-10 22:51:38 +0300

    Add event listener to comments div

--- app/javascript/channels/comments_channel.js
+++ app/javascript/channels/comments_channel.js
@@ -10,6 +10,6 @@ consumer.subscriptions.create("CommentsChannel", {
   },
 
   received(data) {
-    // Called when there's incoming data on the websocket for this channel
+    $('#messages').prepend(data)
   }
 });
