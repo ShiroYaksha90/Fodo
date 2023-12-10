889ccc53beed6e3624107bde85f89596ce9296a7
ShiroYaksha90 <aljazaeribasim@gmail.com>
2023-12-10 22:51:12 +0300

    Add the current user

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,7 +1,9 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
-    dentified_by :current_user
-    def connect; end
+    identified_by :current_user
+    def connect
+      self.current_user = find_current_user
+    end
 
     def disconnect; end
 
