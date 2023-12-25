45423f7a51c68e64a2bcb3e2c8b84ac4cefd1a42
ShiroYaksha90 <aljazaeribasim@gmail.com>
2023-12-25 14:34:31 +0300

    Enable submit tag in action view

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,6 @@ module Foodo
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.action_cable.mount_path = "/cable"
+    config.action_view.automatically_disable_submit_tag = false
   end
 end
